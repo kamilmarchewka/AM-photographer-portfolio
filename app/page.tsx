c05c92e92ab8a3919df52cdef74f8ef2d8bfea11
Kamil <kamilm2003@gmail.com>
2024-05-19 19:25:59 +0200

    Home Page restructure I

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import MainHeader from "@/components/MainHeader";
 import PhotosSections from "@/components/PhotosSections";
 import MainTl from "@/components/MainTl";
+import HomePage from "@/components/navigation/home-page/HomePage";
 
 export default function Home() {
   return (
     <main className="">
-      <MainTl />
+      <HomePage />
     </main>
   );
 }
